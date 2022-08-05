160ed86c64a62feb0069deea5eb4f7dfc5adcb65
shemian <shemian092@gmail.com>
2022-08-05 11:47:43 -0700

    Commit the unversioned files

---
+++ resources/views/admin/tenant/create.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.master')
+
+@section('content')
+
+<h6>Create Tenant</h6>
+<section>
+    <div class="card mt-4">
+        <div class="card-header ">
+            <h5>Tenant Details</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('tenant-store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <label for="name">Full Name</label>
+                        <input type="text" name="name" class="form-control" placeholder="Tenant Full name">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control" placeholder="Enter Email" >
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <label for="Phone">Phone Number</label>
+                        <input type="text" name="phone" class="form-control" placeholder="Enter Email" >
+                    </div>
+                    <div class="col-md-6">
+                        <label for="id_number">Id Number/ Passport Number</label>
+                        <input type="text" name="id_number" class="form-control" placeholder="Id Number/Passport No" >
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <label for="image">tenant Image</label>
+                        <input type="file" name="image" class="form-control" placeholder="upload Your Image">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="image_identity">Identity document</label>
+                        <input type="file" name="image_identity" class="form-control" placeholder="Upload Id/Passport Image" >
+                    </div>
+                    <div class="col-md-6 mt-4">
+                        <label for="address">Address</label>
+                        <input type="text" name="address" class="form-control">
+                    </div>
+                </div>
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <label for="occupation_status">Occupation Status</label>
+                        <input type="text" name="occupation_status" class="form-control">
+                        
+                    </div>
+                    
+                    <div class="col-md-6">
+                        <label for="occupation_place">Occupation Place</label>
+                        <input type="text" name="occupation_place" class="form-control">
+                    </div>
+                </div>
+                
+                <div class="row mt-4">
+                    <div class="col-md-6">
+                        <label for="emergency_contact_person">Emergency Person Name</label>
+                        <input type="text" name="emergency_contact_person" class="form-control">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="emergency_contact_number">Emergency Person Number</label>
+                        <input type="text" name="emergency_contact_number" class="form-control">
+                    </div>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-6">
+                        <button type="submit" class="btn btn-primary" >Save Tenant</button>
+                    </div>
+                </div>
+               
+        
+                
+            </form>
+        </div>
+    </div>
+</section>
+@endsection()
---
+++ resources/views/admin/tenant/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.master')
+
+@section('content')
+
+<h2>view Tenant</h2>
+@endsection()
