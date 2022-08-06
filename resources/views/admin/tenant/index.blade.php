cf8b483cddb2ade5678d11736fe91af819d3ddeb
shemian <shemian092@gmail.com>
2022-08-06 14:38:27 -0700

    Tenant index page for tenants to display their data through data tables

--- resources/views/admin/tenant/index.blade.php
+++ resources/views/admin/tenant/index.blade.php
@@ -2,5 +2,52 @@
 
 @section('content')
 
-<h2>view Tenant</h2>
+@section('styles')
+<h1>jhfhjh</h1>
+
 @endsection()
+<div class="container mt-5">
+    <div class="card mt-4">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-9">
+                    <h6 class="mb-2">View Tenants</h6>
+                </div>
+                <div class="col-md-3">
+                   <a class="mb-2 btn btn-primary" href="{{route('tenant-create')}}" role="button">Add Tenant</a>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered yajra-datatable">
+                <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Id Number</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    
+   
+</div>
+
+
+
+@endsection()
+
+
+@section('scripts')
+
+<script >
+    alert('this')
+</script>
+@endsection()
+
