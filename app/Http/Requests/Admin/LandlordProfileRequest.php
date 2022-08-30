4fb73675a4519dd38686c052df55178c4dc89ce1
shemian <shemian092@gmail.com>
2022-08-30 13:31:35 -0700

    Landlord Profile Request used to protect the fillables in the table

---
+++ app/Http/Requests/Admin/LandlordProfileRequest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LandlordProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'name'=>[
+                'required',
+                'string',
+                'max:200'
+            ],
+            'email'=>[
+                'required',
+                'string',
+                'max:200'
+            ],
+            'phone'=>[
+                'required',
+                'string',
+                'max:10'
+            ],
+            'id_number'=>[
+                'required',
+                'string',
+                'max:10'
+            ],
+
+            'image_identity'=>[
+                'nullable',
+                'mimes:jpg,png,jpeg'
+            ],
+            'address'=>[
+                'required',
+                'string',
+                'max:20'
+            ],
+            'bank_associated'=>[
+                'string',
+                'max:20',
+            ],
+            'bank_account'=>[
+                'string',
+               
+            ]
+        ];
+
+        return $rules;
+    }
+}
