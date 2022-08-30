2fb6b10a5d61424766c7ffe59c1e99dc46e94bb9
shemian <shemian092@gmail.com>
2022-08-30 13:30:04 -0700

    Landlord_profiles table created in the database

---
+++ database/migrations/2022_08_30_201822_create_landlord_profiles_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLandlordProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('landlord_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('id_number');
+            $table->string('image_identity')->nullable();
+            $table->string('address');
+            $table->string('bank_associated');
+            $table->string('bank_account');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('landlord_profiles');
+    }
+}
