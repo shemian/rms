d4d97d8cbcd16552f44f417dd145355f5e12e7d6
shemian <shemian092@gmail.com>
2022-09-01 14:52:59 -0700

    Create landlords Models to insert required data for landlords in the database

--- app/Models/Admin/LandlordProfile.php
+++ app/Models/Admin/LandlordProfile.php
@@ -8,16 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class LandlordProfile extends Model
 {
     use HasFactory;
-    protected $table ='landlord_profiles';
-    protected $fillables =[
+    protected $table='landlord_profiles';
+    protected $fillables=[
         'name',
         'email',
         'phone',
         'id_number',
-        'image',
         'image_identity',
         'address',
         'bank_associated',
         'bank_account',
     ];
+    
 }
