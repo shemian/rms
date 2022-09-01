ab725d970cdece021757bec03efe2e1e3eea4fad
shemian <shemian092@gmail.com>
2022-09-01 14:53:39 -0700

    Landlord Profile controller to manage the CRUD for landlords

--- app/Http/Controllers/Admin/LandlordProfileController.php
+++ app/Http/Controllers/Admin/LandlordProfileController.php
@@ -4,6 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Log;
+use Datatables;
+use App\Models\Admin\LandlordProfile;
+use App\Http\Requests\Admin\LandlordProfileRequest;
+
 
 class LandlordProfileController extends Controller
 {
@@ -13,5 +18,31 @@ class LandlordProfileController extends Controller
 
     public function create(){
         return view ('admin.landlord.create');
+    }
+
+    public function store(LandlordProfileRequest $request){
+       
+        $data = $request->validated();
+       
+       
+        $landlordProfile = new LandlordProfile();
+        $landlordProfile->name =$data['name'];
+        $landlordProfile->email =$data['email'];
+        $landlordProfile->phone =$data['phone'];
+        $landlordProfile->id_number =$data['id_number'];
+
+        if($request->hasfile('image_identity')){
+            $fileimg = $request->file('image_identity');
+            $filenames = time().'.'.$fileimg->getClientOriginalExtension();
+            $fileimg->move('upload/landlordImg/', $filenames);
+            $landlordProfile->image_identity = $filenames;
+        }
+        $landlordProfile->address =$data['address'];
+        $landlordProfile->bank_associated =$data['bank_associated'];
+        $landlordProfile->bank_account =$data['bank_account'];
+        $landlordProfile->save();
+
+        return redirect()->route('landlord-index');
+
     }
 }
