1ff518bd3c5d226d8a31e3d8515d14259dbebc24
shemian <shemian092@gmail.com>
2022-10-10 14:00:36 -0700

    Create new property page

--- resources/views/admin/property/create.blade.php
+++ resources/views/admin/property/create.blade.php
@@ -2,7 +2,71 @@
 
 @section('content')
 
-<h6>Create New Property</h6>
+@section('styles')
 
+<link rel="stylesheet" href="{{ asset('css/demo1/style.min.css') }}">
+
+@endsection
+
+<div class="page-content">
+
+
+<div class="row">
+    <div class="col-md-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Create New Property</h4>
+
+<div id="wizard">
+  <h2>Basic Details</h2>
+  <section>
+    <h4>First Step</h4>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut nulla nunc. Maecenas arcu sem, hendrerit a tempor quis, 
+        sagittis accumsan tellus. In hac habitasse platea dictumst. Donec a semper dui. Nunc eget quam libero. Nam at felis metus. 
+        Nam tellus dolor, tristique ac tempus nec, iaculis quis nisi.</p>
+  </section>
+
+  <h2>Location</h2>
+  <section>
+    <h4>Second Step</h4>
+    <p>Donec mi sapien, hendrerit nec egestas a, rutrum vitae dolor. Nullam venenatis diam ac ligula elementum pellentesque. 
+        In lobortis sollicitudin felis non eleifend. Morbi tristique tellus est, sed tempor elit. Morbi varius, nulla quis condimentum 
+        dictum, nisi elit condimentum magna, nec venenatis urna quam in nisi. Integer hendrerit sapien a diam adipiscing consectetur. 
+        In euismod augue ullamcorper leo dignissim quis elementum arcu porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Vestibulum leo velit, blandit ac tempor nec, ultrices id diam. Donec metus lacus, rhoncus sagittis iaculis nec, malesuada a diam. 
+        Donec non pulvinar urna. Aliquam id velit lacus.</p>
+  </section>
+
+  <h2>Features&Amenities</h2>
+  <section>
+    <h4>Third Step</h4>
+    <p>Morbi ornare tellus at elit ultrices id dignissim lorem elementum. Sed eget nisl at justo condimentum dapibus. Fusce eros justo, 
+        pellentesque non euismod ac, rutrum sed quam. Ut non mi tortor. Vestibulum eleifend varius ullamcorper. Aliquam erat volutpat. 
+        Donec diam massa, porta vel dictum sit amet, iaculis ac massa. Sed elementum dui commodo lectus sollicitudin in auctor mauris 
+        venenatis.</p>
+  </section>
+
+  <h2>Property Image</h2>
+  <section>
+    <h4>Fourth Step</h4>
+    <p>Quisque at sem turpis, id sagittis diam. Suspendisse malesuada eros posuere mauris vehicula vulputate. Aliquam sed sem tortor. 
+        Quisque sed felis ut mauris feugiat iaculis nec ac lectus. Sed consequat vestibulum purus, imperdiet varius est pellentesque vitae. 
+        Suspendisse consequat cursus eros, vitae tempus enim euismod non. Nullam ut commodo tortor.</p>
+  </section>
+</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</div>
 
 @endsection()
+
+@section('scripts')
+
+
+
+@endsection
+
