644f06d3702fcc053284741cebccbdf26609e50e
shemian <shemian092@gmail.com>
2023-01-24 13:44:53 -0800

    Updated the property controller

--- app/Http/Controllers/Admin/PropertyController.php
+++ app/Http/Controllers/Admin/PropertyController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin\LandlordProfile;
+use App\Models\Admin\Property;
+use App\Http\Requests\Admin\PropertyRequest;
 
 class PropertyController extends Controller
 {
     //
+
+
 
     public function index(){
         return view('admin.property.index');
