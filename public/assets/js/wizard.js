d0f8fa54de82c884f6a38d997b5da9b5569503c0
shemian <shemian092@gmail.com>
2023-01-24 13:47:23 -0800

    get the vertical flow of the wizard js form

--- public/assets/js/wizard.js
+++ public/assets/js/wizard.js
@@ -8,11 +8,5 @@ $(function() {
     bodyTag: "section",
     transitionEffect: "slideLeft"
   });
-
-  $("#wizardVertical").steps({
-    headerTag: "h2",
-    bodyTag: "section",
-    transitionEffect: "slideLeft",
-    stepsOrientation: 'vertical'
-  });
+  
 });
